e89154157686769f18d9bbc042d33bb22835b2ec
Amiraali1995 <mirolicious95@hotmail.com>
2023-07-26 15:17:45 +0400

    day four update 2

--- exercise_2.py
+++ exercise_2.py
@@ -1,10 +1,23 @@
 #exercise 2
-total_width=100
-tile_width = 5
 #* calculate total width of all tiles
    #**one black tle:5
    #**9 pair of BWs:90
    #**total width:95
 #* calculate gap (one on each end):
    #**100-95=5 total gap
-   #ggap/2=2.5 at each end 
+   #ggap/2=2.5 at each end
+total_width=100
+n=0
+black_w=5
+white_w=5
+
+total_width=total_width- black_w
+
+while total_width > 0:
+    total_width=total_width-white_w-black_w
+    n +=1
+    if total_width <0:
+        break
+total_width = total_width+white_w+black_w
+print("The gap =",total_width/2)
+print("number of tails =",n*2-1)
